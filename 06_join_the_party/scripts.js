76f85235ec000b9247663f41fcdf675fa5f106f7
jayeanleiquina <jayeanleiquina@shgcham.school.nz>
2026-06-15 13:57:56 +1200

    lesson 6 doneeeeeeeee (or 7 i cant remember)

--- 06_join_the_party/scripts.js
+++ 06_join_the_party/scripts.js
@@ -1,10 +1,14 @@
 let nameInput = document.getElementById("nameInput");
+let roleInput = document.getElementById("roleInput");
 let greetButton = document.getElementById("greetButton");
 let output = document.getElementById("output");
+let partyTheme = "first JavaScript party";
 
 function sayHello() {
   let name = nameInput.value;
-  output.innerText = "Welcome, " + name;
+  let role = roleInput.value;
+  output.innerText = "Welcome, " + name + " the " + role + " to your " + partyTheme.toUpperCase() + "!";
 }
 
 greetButton.onclick = sayHello;
+
